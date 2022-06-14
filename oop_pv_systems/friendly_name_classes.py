eb139fe8710b093e44c40e8d6b837299a050c48f
jiasheng <projiasheng98@gmail.com>
2022-06-14 16:23:29 +0800

    include simple docstring

--- oop_pv_systems/friendly_name_classes.py
+++ oop_pv_systems/friendly_name_classes.py
@@ -1,7 +1,11 @@
 import random
 
 
-class FriendlyName:
+RANDOM_SEED = 98
+random.seed(RANDOM_SEED)  # This helps to reproduce results
+
+
+class FriendlyName(object):
     color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "purple", "pink"]
     verb = ["playing", "jumping", "eating", "working", "studying", "driving", "walking", "writing", "reading", "talking"]
     noun = ["Ava", "Brooklyn", "Charlotte", "Delilah", "Emma", "Faith", "Grace", "Henry", "Isaac", "James"]
@@ -9,6 +13,12 @@ class FriendlyName:
     friendly_name_list = []
 
     def __init__(self):
+        """
+        The __init__ function is called automatically every time the class is instantiated.
+
+        param :self: Represent the instance of the class
+        return: a friendly name as an object
+        """
         self.name = None
         self.counter = 1
         self.counter_limit = 3
@@ -29,6 +39,14 @@ class FriendlyName:
         return f'friendly name: {self.name}'
 
     def generate_friendly_name(self):
+        """
+        The generate_friendly_name function generates a friendly name.
+        It chooses a color and a verb from lists of colors, verbs and names, respectively.
+        Then it concatenates them together to form the friendly name.
+
+        param self: Access variables that belongs to the class
+        return: a friendly name as a string
+        """
         self.color = random.choice(FriendlyName.color)
         self.verb  = random.choice(FriendlyName.verb)
         self.noun  = random.choice(FriendlyName.noun)
@@ -36,6 +54,14 @@ class FriendlyName:
 
     @staticmethod
     def is_unique(friendly_name: str, friendly_name_list: list):
+        """
+        The is_unique function checks to see if the friendly name is already in use.
+        If it is not, then True will be returned. If it is, False will be returned.
+
+        param friendly_name: str: Check if the friendly_name has already been generated
+        param friendly_name_list: list: A cumulative list of all the friendly_name generated
+        return: True if the friendly_name is not in the friendly_name list
+        """
         if (any(friendly_name_list) is False) or (friendly_name not in friendly_name_list):
             return True
         else:
